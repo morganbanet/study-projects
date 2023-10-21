c55055428d8c15d5a29aea918dd98d8bc981cd4a
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-10-21 04:33:53 +0100

    React Conditional Rendering - p115

--- react/hacker-stories/src/App.jsx
+++ react/hacker-stories/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // Custom hook
 const useStorageState = (key, initialState) => {
@@ -12,7 +12,7 @@ const useStorageState = (key, initialState) => {
 };
 
 const App = () => {
-  const stories = [
+  const initialState = [
     {
       title: 'React',
       url: 'https://reactjs.org/',
@@ -39,7 +39,32 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [searchTerm, setSearchTerm] = useStorageState('search', '');
+  const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const getAsyncStories = () =>
+    new Promise((resolve) =>
+      setTimeout(() => resolve({ data: { stories: initialState } }), 2000)
+    );
+
+  useEffect(() => {
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
+  }, []);
+
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => story.objectId !== item.objectId
+    );
+
+    setStories(newStories);
+  };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -57,6 +82,7 @@ const App = () => {
         id="search"
         label="Search"
         value={searchTerm}
+        isFocused // Attributes like this shorthand for true
         onInputChange={handleSearch}
       >
         <strong>Search:</strong>
@@ -64,7 +90,13 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} />
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
@@ -75,26 +107,41 @@ const InputWithLabel = ({
   value,
   type = 'text',
   onInputChange,
+  isFocused,
 }) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
   return (
     <>
       <label htmlFor={id}>{children}</label> &nbsp;
-      <input id={id} type={type} value={value} onChange={onInputChange} />
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
     </>
   );
 };
 
-const List = ({ list }) => {
+const List = ({ list, onRemoveItem }) => {
   return (
     <ul>
       {list.map((item) => (
-        <Item key={item.objectId} item={item} />
+        <Item key={item.objectId} item={item} onRemoveItem={onRemoveItem} />
       ))}
     </ul>
   );
 };
 
-const Item = ({ item }) => {
+const Item = ({ item, onRemoveItem }) => {
   return (
     <li>
       <span>
@@ -103,6 +150,11 @@ const Item = ({ item }) => {
       <span>{item.author}</span>
       <span>{item.num_comments}</span>
       <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
     </li>
   );
 };
