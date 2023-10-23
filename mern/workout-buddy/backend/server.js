83d9d0b9a8472b807c7fa67570cf787bf575cbde
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-10-23 08:15:11 +0100

    Modified mongoose connect

--- mern/workout-buddy/backend/server.js
+++ mern/workout-buddy/backend/server.js
@@ -1,27 +1,37 @@
 require('dotenv').config({ path: './config/config.env' });
-const connectDB = require('./config/database');
+
+const mongoose = require('mongoose');
 const express = require('express');
 const morgan = require('morgan');
 
-// Route imports
 const workoutRoutes = require('./routes/workoutRoutes');
 
 // Express config
-connectDB();
-const PORT = process.env.PORT || 5000;
 const app = express();
-
-// Body parsing
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+const PORT = process.env.PORT || 5000;
 
 // Morgan config
 app.use(morgan('dev'));
 
+// Body parsers
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 // Route mounts
 app.use('/api/workouts', workoutRoutes);
 
-// Run server
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+// Connect database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then((conn) => {
+    console.log(`Database connected: ${conn.connection.host}`);
+
+    // Run server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
