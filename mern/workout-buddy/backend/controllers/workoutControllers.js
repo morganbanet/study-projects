66ab67e4de7f385acca431712bab478da83d606b
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-10-23 06:16:53 +0100

    Added asyncHandler middleware

--- mern/workout-buddy/backend/controllers/workoutControllers.js
+++ mern/workout-buddy/backend/controllers/workoutControllers.js
@@ -1,34 +1,36 @@
+const asyncHandler = require('../middleware/asyncMiddleware');
+
 // @desc        Get workouts
 // @route       GET /api/workouts
 // @access      Private
-exports.getWorkouts = (req, res) => {
+exports.getWorkouts = asyncHandler(async (req, res) => {
   return res.status(200).json({ success: true, message: 'Get workouts' });
-};
+});
 
 // @desc        Get workout
 // @route       GET /api/workouts/:id
 // @access      Private
-exports.getWorkout = (req, res) => {
+exports.getWorkout = asyncHandler(async (req, res) => {
   return res.status(200).json({ success: true, message: 'Get workout' });
-};
+});
 
 // @desc        Create workout
 // @route       POST /api/workouts
 // @access      Private
-exports.createWorkout = (req, res) => {
+exports.createWorkout = asyncHandler(async (req, res) => {
   return res.status(201).json({ success: true, message: 'Create workout' });
-};
+});
 
 // @desc        Update workout
 // @route       PATCH /api/workouts/:id
 // @access      Private
-exports.updateWorkout = (req, res) => {
+exports.updateWorkout = asyncHandler(async (req, res) => {
   return res.status(200).json({ success: true, message: 'Update workout' });
-};
+});
 
 // @desc        Delete workout
 // @route       DELETE /api/workouts/:id
 // @access      Private
-exports.deleteWorkout = (req, res) => {
+exports.deleteWorkout = asyncHandler(async (req, res) => {
   return res.status(200).json({ success: true, message: 'Delete workout' });
-};
+});
