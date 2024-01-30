7c691dc99c0dd4b353a8c05099e3205818f1398c
morganbanet <137121096+morganbanet@users.noreply.github.com>
2024-01-30 02:05:45 +0000

    Initialized Steve Griffith puppeteer tutorial project

--- puppeteer/steve-griffith-puppeteer-headless/basic.js
+++ puppeteer/steve-griffith-puppeteer-headless/basic.js
@@ -2,3 +2,9 @@
 // npm init -y
 // add "type":"module" to package.json
 // npm install puppeteer@19.11.1
+
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+})();
